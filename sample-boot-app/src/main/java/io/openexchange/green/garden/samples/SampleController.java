6b5376cf51beefd648adc27297ef0a3802828ec6
Vinay Balamuru <vybe3142@gmail.com>
2018-05-10 06:46:19 -0500

    Enable google cloud deployment (mvn dependencies and downgrade to java9 (highest supported depoyment version)) , add a simple MVC controller

---
+++ sample-boot-app/src/main/java/io/openexchange/green/garden/samples/SampleController.java
@@ -0,0 +1,16 @@
+package io.openexchange.green.garden.samples;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public String hello() throws UnknownHostException {
+        return "Hello !! " + InetAddress.getLocalHost().getHostName() ;
+    }
+}
